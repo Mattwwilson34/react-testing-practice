71230415e4448a19559e4b21a80b85689cb9445e
Matt Wilson <mattwwilson34@gmail.com>
2022-03-09 20:56:49 -0500

    Removed tests to use file with academind.com/tutorials

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-// App.js
+import React from 'react';
 
-import React, { useState } from 'react';
-
-const App = () => {
-  const [heading, setHeading] = useState('Magnificent Monkeys');
-
-  const clickHandler = () => {
-    setHeading('Radical Rhinos');
-  };
-
+function App() {
   return (
-    <>
-      <button type='button' onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
-};
+}
 
 export default App;
